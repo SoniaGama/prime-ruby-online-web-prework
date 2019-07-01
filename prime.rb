50dd15bd56dba3f90451c5e40263b162ae13cbca
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-07-01 01:29:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(numero)
+  numero_auxiliar = 0
+  
+  1.step numero do |n|
+    if numero % n == 0
+      numero_auxiliar += 1
+    end
+  
+  if numero_auxiliar == 2
+    true 
+  else
+    false 
+  end
+end
+
+
+if aux == 2
+    puts "\nEl número ingresado es primo!!"
+else
+    puts "\nEl número ingresado no es primo!!"
+end
