e27a937f05043577da23fed39e1a79139b206f72
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-07-01 01:30:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,17 +5,10 @@ def prime?(numero)
     if numero % n == 0
       numero_auxiliar += 1
     end
-  
+  end 
   if numero_auxiliar == 2
     true 
   else
     false 
   end
-end
-
-
-if aux == 2
-    puts "\nEl número ingresado es primo!!"
-else
-    puts "\nEl número ingresado no es primo!!"
 end
